b2f35290fc9a4ff59bf3cf367511986b67cbef7f
Konrad Ciochoń <d00d171@gmail.com>
2015-04-13 20:37:46 +0200

    test

--- server/models/models.js
+++ server/models/models.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 function makeModel(schemaDefinition){
 	return mongoose.model(schemaDefinition.name, schemaDefinition.schema)
 }
-
+ 
 module.exports = {
 
 	Slideshow : makeModel(schemaDefinitions.slideshow),
