45187eaabf632ec8cbc170a6ad4995f5c575e776
iosrSlideshows <iosrslidesteam@gmail.com>
2015-04-28 10:51:37 +0200

    Update auth_config.js

--- server/config/auth_config.js
+++ server/config/auth_config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     clientID: '882422898806-qpsu75muaeg1p7sctcpf09gmsqd5qlos.apps.googleusercontent.com',
     clientSecret: 'RFabMgD6IJIltkN-DbM9SsQ5',
-    callbackURL:  'http://localhost:8080/auth/google/callback'
+    callbackURL:  'http://172.17.84.85:8080/auth/google/callback'
 
 };
 
